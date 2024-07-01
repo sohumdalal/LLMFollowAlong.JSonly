4f96e3b104f4436221596d84139fa32bfe4b2d13
sohumdalal <dsohum@gmail.com>
2024-06-30 20:29:40 -0400

    client adjusts

---
+++ client/client/src/components/ChatInterface.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChatInterface = () => {
+  return (
+    <div>ChatInterface</div>
+  )
+}
+
+export default ChatInterface
---
+++ client/client/src/components/UrlInput.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UrlInput = () => {
+  return (
+    <div>UrlInput</div>
+  )
+}
+
+export default UrlInput
